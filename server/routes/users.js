12afa7e53ae43d63026f50d2d0dd187e2c6ed0d5
semperry <ryan.curtis88@gmail.com>
2019-06-04 20:35:50 -0600

    password hashing enabled and registration validations in place

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,8 +1,36 @@
 const router = require('express').Router()
 const User = require('../models/User')
+const bcrypt = require('bcryptjs')
+const validateRegisterInput = require('../validation/register')
 
-router.route('/register').get((req, res) => {
-  res.send('Hello')
+router.route('/register').post((req, res) => {
+  const { isValid, errors } = validateRegisterInput(req.body)
+
+  if(!isValid) {
+    return res.status(500).json(errors)
+  }
+
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (user) {
+        errors.email = 'Account already exists with this email'
+        return res.status(500).json(errors)
+      }
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(req.body.password, salt, (err, hash) => {
+          const newUser = new User({
+            email: req.body.email, 
+            login: req.body.login,
+            password: hash
+          })
+
+          newUser.save()
+            .then(newUser => res.json(newUser))
+            .catch(err => console.log("Registration error: " + err))
+        })
+      })
+    })
 })
 
 module.exports = router
--- server/validation/register.js
+++ server/validation/register.js
@@ -1,1 +1,44 @@
 const Validator = require('validator')
+
+module.exports = data => {
+  let errors = {}
+  
+  if(Validator.isEmpty(data.email)){
+    errors.email = 'Email field is required'
+  }
+
+  if(!Validator.isEmail(data.email)){
+    errors.email = 'Email is invalid'
+  }
+
+  if(Validator.isEmpty(data.login)){
+    errors.login = 'Login field is required'
+  }
+
+  if (!Validator.isLength(data.login, { min: 4, max: 30 })) {
+    errors.login = 'Password must be between 4 and 30 characters'
+  }
+
+  if(Validator.isEmpty(data.password)){
+    errors.email = 'Password field is required'
+  }
+
+  if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
+    errors.password = 'Password must be between 6 and 30 characters'
+  }
+
+  if (Validator.isEmpty(data.passwordTwo)) {
+    errors.passwordTwo = 'Please confirm your password'
+  }
+  
+  if (!Validator.equals(data.password, data.passwordTwo)) {
+    errors.passwordTwo = 'Passwords must match'
+  }
+
+  return {
+    errors, 
+    isValid: Object.keys(errors).length === 0
+  }
+
+  
+}
