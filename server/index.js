d3a520981236dda4e8378b326a9fdece1638a656
semperry <ryan.curtis88@gmail.com>
2019-06-11 10:50:02 -0600

    Passport added to server.

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const users = require('./routes/users')
 const PORT = 5000 || process.env.PORT
-
+const passport = require('passport')
 
 const app = express()
 
@@ -25,6 +25,9 @@ mongoose.connect(process.env.MONGOLAB_URI, { useNewUrlParser: true })
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cors())
+
+app.use(passport.initialize())
+require('./config/passport')(passport)
 
 app.use('/api/users', users)
 
